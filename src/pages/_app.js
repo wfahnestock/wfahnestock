c2d687e11fc2a79a211328bedbed37a6f38cfd94
wfahnestock <wfahnestock@student.fullsail.edu>
2021-10-24 20:36:27 -0400

    Add NextSEO for Search Engine Optimization

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,14 @@
 import Theme from '../styles/theme';
+import { NextSeo } from 'next-seo';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Theme>
+        <NextSeo
+          title="Will Fahnestock | Portfolio"
+          description="Will Fahnestock is a Web Developer with a B.Sc in Web Design & Development from Full Sail University."
+        />
         <Component {...pageProps} />
       </Theme>
     </>
